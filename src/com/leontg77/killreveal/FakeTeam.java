11c6b95f1da48656f976cea094cee12c5a04b30a
LeonTG77 <leontg77@gmail.com>
2016-01-02 13:14:10 +0100

    Added JavaDocs

--- src/com/leontg77/killreveal/FakeTeam.java
+++ src/com/leontg77/killreveal/FakeTeam.java
@@ -1,39 +1,92 @@
 package com.leontg77.killreveal;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.OfflinePlayer;
 
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * FakeTeam class.
+ * <p>
+ * Class used to store teammates for kill reveal since 
+ * vanilla teams sorts the tab list.
+ * 
+ * @author LeonTG77
+ */
 public class FakeTeam {
-	private List<UUID> players = new ArrayList<UUID>();
-	int id;
+	private Set<UUID> players = new HashSet<UUID>();
+	private int id;
 	
+	/**
+	 * FakeTeam constructor.
+	 * 
+	 * @param id the id of the team.
+	 */
 	public FakeTeam(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * Get the ID of this team.
+	 * 
+	 * @return The ID.
+	 */
 	public int getId() {
 		return id;
 	}
 
-	public List<UUID> getPlayers() {
-		return players;
+	/**
+	 * Get a set of all UUID's on this team.
+	 * 
+	 * @return A set of UUID's.
+	 */
+	public Set<UUID> getPlayers() {
+		return ImmutableSet.copyOf(players);
 	}
 
-	public int getSize() throws IllegalStateException {
+	/**
+	 * Get the size of the team.
+	 * 
+	 * @return The size.
+	 */
+	public int getSize() {
 		return players.size();
 	}
 
+	/**
+	 * Add the given player to the team.
+	 * 
+	 * @param player The player adding.
+	 */
 	public void addPlayer(OfflinePlayer player) {
+		for (FakeTeam team : Main.getFakeTeams()) {
+			if (team.hasPlayer(player)) {
+				team.removePlayer(player);
+			}
+		}
+		
 		players.add(player.getUniqueId());
 	}
-
+	
+	/**
+	 * Remove the given player to the team.
+	 * 
+	 * @param player The player removing.
+	 * @return True if successful, false otherwise.
+	 */
 	public boolean removePlayer(OfflinePlayer player) {
 		return players.remove(player.getUniqueId());
 	}
 
+	/**
+	 * Check if the given player is on this team.
+	 * 
+	 * @param player The player checking.
+	 * @return True if he is, false otherwise.
+	 */
 	public boolean hasPlayer(OfflinePlayer player) {
 		return players.contains(player.getUniqueId());
 	}
